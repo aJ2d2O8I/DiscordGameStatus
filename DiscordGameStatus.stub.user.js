aa90891f192db9da100d6fc7a79f7022366f7347
aJ2d2O8I <38075783+aJ2d2O8I@users.noreply.github.com>
2018-04-04 17:11:46 +0300

    for ptb. and canary. subdomains
    would

--- DiscordGameStatus.stub.user.js
+++ DiscordGameStatus.stub.user.js
@@ -4,9 +4,11 @@
 // @name        DiscordGameStatus
 // @description A userscript for setting the currently playing game in the Discord web client
 // @include     https://discordapp.com/*
+// @include     https://ptb.discordapp.com/*
+// @include     https://canary.discordapp.com/*
 // @version     0
 // @grant       GM_unsafeWindow
 // @run-at      document-start
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.user.js
 // @updateURL   https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.meta.js
-// ==/UserScript==
+// + ==/UserScript==
